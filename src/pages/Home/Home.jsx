d42cbf8510329719bd43683e7e1aae0b037d787e
Raghadalalai1 <144120085+Raghadalalai1@users.noreply.github.com>
2024-06-16 11:52:07 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import './Home.css'
 import Navbarr from '../../components/Navbarr/Navbarr'
 import Footer from '../../components/Footer/Footer'
-
-
 import Section from '../../components/Section/Section6'
+
+
 
 export default function Home() {
   return (
@@ -12,14 +12,14 @@ export default function Home() {
      <Navbarr />
     <div className='main'>
     
-      
+        <Section />
+      <Footer />
    
       
       
       
       
-      <Section />
-      <Footer />
+    
       
       
        
