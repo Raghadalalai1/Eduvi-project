86efeb52e1e9c19a2157ad325fffb1e1ec985f6d
Raghadalalai1 <144120085+Raghadalalai1@users.noreply.github.com>
2024-06-16 12:34:43 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,30 +3,24 @@ import './Home.css'
 import Navbarr from '../../components/Navbarr/Navbarr'
 import Footer from '../../components/Footer/Footer'
 import Section from '../../components/Section/Section6'
-
-
+import KGHeroHome from '../../components/KGHeroHome/KGHeroHome'
 
 export default function Home() {
   return (
     <>
      <Navbarr />
     <div className='main'>
-    
+
+       <KGHeroHome />
         <Section />
       <Footer />
    
       
-      
-      
-      
-    
-      
-      
-       
-      
-
-        
+  
     </div>
+    </>
+  )
+}
 
    
     </>
