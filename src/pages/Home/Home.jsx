e73d6d1ffa3c77009841056607b6ad72bebb0dae
Raghadalali1 <raghadalali63@gmail.com>
2024-06-16 13:54:44 +0300

    Add new component section2shope

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 import Navbarr from '../../components/Navbarr/Navbarr'
 import Footer from '../../components/Footer/Footer'
 import KGHeroHome from '../../components/KGHeroHome/KGHeroHome'
+
 export default function Home() {
   return (
     <>
