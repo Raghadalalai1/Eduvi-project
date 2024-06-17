032933cf84d9057ad7ac04601e9192a30cb5e585
unknown <ghanemkinda@gmail.com>
2024-06-17 15:01:07 +0300

    update KGCardsBook

--- src/components/KGCardsBooks/KGCardsBooks.jsx
+++ src/components/KGCardsBooks/KGCardsBooks.jsx
@@ -6,18 +6,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function KGCardsBooks({KGTitle}) {
     const KGCardsBooks = [
      {
+        id : 1 ,
         Image : './public/images/Shop/Shop-Section/Small-Image-1.svg' ,
         stars : < FontAwesomeIcon icon={faStar} className='KG-Star-color' /> ,
         text : "The Three Musketeers, by Alexandre Dumas" ,
         price : '39$'
     },
     {
+        id : 2 ,
         Image : './public/images/Shop/Shop-Section/Small-Image-2.svg' ,
         stars : < FontAwesomeIcon icon={faStar}  className='KG-Star-color' /> ,
         text : "The Three Musketeers, by Alexandre Dumas" ,
         price : '39$'
     } ,
     {
+        id : 3 ,
         Image : './public/images/Shop/Shop-Section/Small-Image-3.svg' ,
         stars : < FontAwesomeIcon icon={faStar} className='KG-Star-color' /> ,
         text : "The Three Musketeers, by Alexandre Dumas" ,
@@ -29,7 +32,7 @@ export default function KGCardsBooks({KGTitle}) {
                 <h3 className='KG-Card-Title'>{KGTitle}</h3>
                 { KGCardsBooks.map((book) => {
                     return(
-                            <div className='KG-Card-Book'>
+                            <div className='KG-Card-Book' key={book.id}>
                                 <div className='KG-Card-Info-Book'>
                                     <img src={book.Image} alt='image-title'></img>
                                     <div>
