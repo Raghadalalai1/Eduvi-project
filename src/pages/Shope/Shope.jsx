ce9a9d83a168247ecd7918248d4a96f63af0866b
Raghadalali1 <raghadalali63@gmail.com>
2024-06-17 14:34:19 +0300

    Add section2shop to shop page

--- src/pages/Shope/Shope.jsx
+++ src/pages/Shope/Shope.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import './Shope.css'
 import Footer from '../../components/Footer/Footer'
 import Navbarr from '../../components/Navbarr/Navbarr'
+import Section2Shop from '../../components/Section2Shop/Section2Shop'
 export default function Shope() {
   return (
     <>
     <Navbarr />
    <div className='main'>
-     
+     <Section2Shop />
      <Footer />
 
        
