6862bf527ddc5be5c9891beba3a2ca096587cd88
Raghadalali1 <raghadalali63@gmail.com>
2024-06-18 00:15:27 +0300

    add changes

--- src/pages/Shope/Shope.jsx
+++ src/pages/Shope/Shope.jsx
@@ -3,12 +3,14 @@ import './Shope.css'
 import Footer from '../../components/Footer/Footer'
 import Navbarr from '../../components/Navbarr/Navbarr'
 import Section2Shop from '../../components/Section2Shop/Section2Shop'
+import Section from '../../components/Section/Section6'
 export default function Shope() {
   return (
     <>
     <Navbarr />
    <div className='main'>
      <Section2Shop />
+     <Section />
      <Footer />
 
        
