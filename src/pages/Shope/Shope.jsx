326a404a33c147100fce010fb697dd88eccb2b12
unknown <ghanemkinda@gmail.com>
2024-06-17 14:39:57 +0300

    addCardBook

--- src/pages/Shope/Shope.jsx
+++ src/pages/Shope/Shope.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './Shope.css'
 import Footer from '../../components/Footer/Footer'
 import Navbarr from '../../components/Navbarr/Navbarr'
-import Section2Shop from '../../components/Section2Shop/Section2Shop'
 export default function Shope() {
   return (
     <>
