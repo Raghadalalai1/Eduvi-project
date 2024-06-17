51dae7947a65126645e9a3dac504069d19a136c2
hindshyouni <hind.shyouni99@gmail.com>
2024-06-18 02:06:38 +0300

    update

--- src/components/HSShopSection2/HSSopSection2.jsx
+++ src/components/HSShopSection2/HSSopSection2.jsx
@@ -1,10 +1,39 @@
+
+import { faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import './HSShopSection2.css'
 
 export default function HSShopSection2() {
   return (
 
-   <h1 className='hh'>HSSopSection2</h1>
+   <div className='HS-LeftSection'>
+    <div className='HS-LeftSection-Button'>
+      <button className='HS-Button1'>All Book</button>
+      <button className='HS-Button2'>Kindergarten</button>
+      <button className='HS-Button3'>High School</button>
+      <button className='HS-Button4'>College</button>
+
+    </div>
+
+    <form className='HS-Form'>
+      <div className='HS-input'>
+      <input type='search' placeholder=' Serach Class, Course, Book Name'></input>
+      <button>
+      < FontAwesomeIcon icon={faMagnifyingGlass}  />
+        </button>
+        </div>
+        <select name='School' className='HS-Search-Select'>
+            <option>Sort by:<span>Latest</span> </option>
+            <option>Sort by: oldest</option>
+            
+          </select>
+      
+
+    </form>
+
+
+   </div>
 
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,7 +17,7 @@ export default function Home() {
 
        <KGHeroHome />
         <Section />
-      <KGHeroHome />
+      
       
       <Footer />
    
