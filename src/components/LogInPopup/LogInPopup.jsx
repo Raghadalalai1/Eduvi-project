5dc70675216554cb405c3751226a645011fd68ba
Raghadalali1 <raghadalali63@gmail.com>
2024-06-18 23:41:32 +0300

    ADD SOME CHANGES

--- src/components/LogInPopup/LogInPopup.jsx
+++ src/components/LogInPopup/LogInPopup.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import './LogInPopup.css'
 import Carousel from 'react-bootstrap/Carousel';
-
+import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons'
+// import { faEnvelope} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 export default function LogInPopup() {
     return (
         <div className='RA-LogInPopup'>
@@ -11,21 +14,45 @@ export default function LogInPopup() {
                     <h2 className='RA-titel-login'>Welcome to
                         Eduvi Online
                         Learning Platform</h2>
-                    <Carousel>
-                        <Carousel.Item interval={1000} className='RA-slider'>
-                            <img src="./images/LogIn/login.svg" alt="HELOO" className='RA-slier-img' />
+                    <Carousel className='slider'>
+                        <Carousel.Item className='RA-slider'>
+                            <img src="./images/LogIn/login.svg" alt="HELOO" className='RA-slier-img  d-block ' />
 
                         </Carousel.Item>
-                        {/* <Carousel.Item interval={500}>
-                            <img src="./images/LogIn/login.svg" alt="HELOO" className='RA-slier-img'/>
+                        <Carousel.Item className='RA-slider'>
+                            <img src="./images/LogIn/login.svg" alt="HELOO" className='RA-slier-img d-block' />
 
-                        </Carousel.Item> */}
-                        {/* <Carousel.Item>
+                        </Carousel.Item>
+                        <Carousel.Item className='RA-slider'>
 
-                            <img src="./images/LogIn/login.svg" alt="HELOO" className='RA-slier-img'/>
-                        </Carousel.Item> */}
+                            <img src="./images/LogIn/login.svg" alt="HELOO" className='RA-slier-img d-block' />
+                        </Carousel.Item>
                     </Carousel>
 
+                </div>
+                <div className='RA-login-form'>
+                    <form action="" >
+                        <div className='ra-CONTANAIR'>
+
+                            <div className='RA-GOOGLE-ICON'>
+                                <FontAwesomeIcon icon={faGooglePlusG} className='Ra-icon' style={{ color: "#ffffff", fontSize: '24px' }} />
+
+                            </div>
+                            <input type="text" placeholder='Sign in with google' className='RA-gool' />
+
+
+
+                        </div>
+                        <p className='RA-signin'>Or signin with your email</p>
+                        <div className='RA-JOIN'>
+                            <label>Email</label>
+                            <FontAwesomeIcon icon={faEnvelope} style={{ color: "#5d5a6f", }} />       <input type="email" placeholder='bill.sanders@example.com' />
+
+                        </div>
+
+
+
+                    </form>
                 </div>
             </div>
         </div>
--- src/components/Section2Shop/Section2Shop.jsx
+++ src/components/Section2Shop/Section2Shop.jsx
@@ -3,11 +3,12 @@ import KGCardsBooks from './../KGCardsBooks/KGCardsBooks'
 
 export default function Section2Shop() {
   return (
-    <div className='RA-S2-Shop'>
+    <div className='RA-S2-Shop' style={{display:'flex' ,justifyContent:'space-between',flexWrap:'wrap' }}>
       <div>
         <KGCardsBooks KGTitle = {'Popular Books'} />
         <KGCardsBooks KGTitle = {'New Arrivals'} />
       </div>
+
       
     </div>
   )
