620ba4431b3789b6f72fd832c51c642524bd9784
ahmadmalek00 <hamadmalok99@gmail.com>
2024-06-19 16:40:41 +0300

    section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,10 +3,10 @@ import './Home.css'
 import Navbarr from '../../components/Navbarr/Navbarr'
 import Footer from '../../components/Footer/Footer'
 
-
 import Section from '../../components/Section/Section6'
 import KGHeroHome from '../../components/KGHeroHome/KGHeroHome'
-
+import AMSEC from '../../components/section-6/AM-SEC'
+import Registation from '../../components/Registation/Regstation'
 export default function Home() {
   return (
     <>
@@ -15,6 +15,9 @@ export default function Home() {
 
 
        <KGHeroHome />
+       <Registation />
+       <AMSEC />
+
         <Section />
       <Footer />
    
