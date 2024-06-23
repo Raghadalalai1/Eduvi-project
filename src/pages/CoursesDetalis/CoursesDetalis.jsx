c5e9f6fd5f8355d4258ad2f0955bd4a87931c5a2
unknown <ghanemkinda@gmail.com>
2024-06-23 16:22:53 +0300

    add KG-Hero-CourseDetails

--- src/pages/CoursesDetalis/CoursesDetalis.jsx
+++ src/pages/CoursesDetalis/CoursesDetalis.jsx
@@ -3,11 +3,13 @@ import './CoursesDetalis.css'
 import Footer from '../../components/Footer/Footer'
 import Navbarr from '../../components/Navbarr/Navbarr'
 import Section from '../../components/Section/Section6'
+import KGCourseDetailsPlayList from '../../components/KGCourseDetailsPlayList/KGCourseDetailsPlayList'
 export default function CoursesDetalis() {
   return (
     <>
     <Navbarr />
    <div className='main'>
+    <KGCourseDetailsPlayList />
      <Section />
      <Footer />
 
