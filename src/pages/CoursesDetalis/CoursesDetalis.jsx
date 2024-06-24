b2f00845d7ce32c9319a5cafb59b9d40e8ae2348
ahmadmalek00 <hamadmalok99@gmail.com>
2024-06-24 15:42:29 +0300

    sec course details

--- src/pages/CoursesDetalis/CoursesDetalis.jsx
+++ src/pages/CoursesDetalis/CoursesDetalis.jsx
@@ -3,11 +3,13 @@ import './CoursesDetalis.css'
 import Footer from '../../components/Footer/Footer'
 import Navbarr from '../../components/Navbarr/Navbarr'
 import Section from '../../components/Section/Section6'
+import AZcompobn from '../../components/Course Detail/AZ-compon'
 export default function CoursesDetalis() {
   return (
     <>
     <Navbarr />
    <div className='main'>
+   <AZcompobn />
      <Section />
      <Footer />
 
