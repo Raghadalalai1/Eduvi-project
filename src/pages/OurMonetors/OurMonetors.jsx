014d262982d45f5e8f66a3428c655d2dd2e41291
unknown <ghanemkinda@gmail.com>
2024-06-26 19:27:06 +0300

    Add LKOurMentor

--- src/pages/OurMonetors/OurMonetors.jsx
+++ src/pages/OurMonetors/OurMonetors.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import './OurMonetors.css'
 import Navbarr from '../../components/Navbarr/Navbarr'
 import Footer from '../../components/Footer/Footer'
+import LKOurMentorssection2 from '../../components/LK-OurMonetors-Section2/LK-OurMonetors-Section2/LK-OurMentors-section2'
 export default function OurMonetors() {
   return (
     <>
     <Navbarr />
    <div className='main'>
+    <LKOurMentorssection2 />
      
      <Footer />
 
