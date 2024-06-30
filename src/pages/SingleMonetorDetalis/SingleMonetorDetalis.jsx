10f137fd29c25e0787f9b461f374ea98abeb0756
unknown <ghanemkinda@gmail.com>
2024-06-30 20:52:20 +0300

    Add KGSinglMentor

--- src/pages/SingleMonetorDetalis/SingleMonetorDetalis.jsx
+++ src/pages/SingleMonetorDetalis/SingleMonetorDetalis.jsx
@@ -3,11 +3,13 @@ import './SingleMonetorDetalis.css';
 import Navbarr from '../../components/Navbarr/Navbarr';
 import Footer from '../../components/Footer/Footer';
 import Section from '../../components/Section/Section6'
+import KGSingleMentor from '../../components/KGSingleMentorDetails/KGSingleMentor';
 export default function SingleMonetorDetalis() {
   return (
     <>
     <Navbarr />
    <div className='main'>
+    <KGSingleMentor />
      <Section />
      <Footer />
 
